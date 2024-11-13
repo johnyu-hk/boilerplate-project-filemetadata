77a1b509f4114aea5d2306226547168f02398f3e
John <johnsonyu97@gmail.com>
2024-11-13 18:29:03 +0800

    feat: multer, return file name type size

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
 var app = express();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  resObj = {};
+  resObj.name = req.file.originalname;
+  resObj.type = req.file.mimetype;
+  resObj.size = req.file.size;
+  // resObj.fileName = req.
+  console.log(resObj);
+  res.json(resObj);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
